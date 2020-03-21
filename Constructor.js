fe4a999d27c4c9114b5458e58e3911b7e7c82253
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-21 04:26:54 -0300

    Se agrega ejercicio constructor

---
+++ Constructor.js
@@ -0,0 +1,9 @@
+class Curso {
+  constructor(titulo, duracion, color="Yelow") {
+    this.titulo = titulo;
+    this.duracion = duracion;
+    this.color = color;
+  }
+}
+
+new Curso("Curso Profesional De JavaScript");
