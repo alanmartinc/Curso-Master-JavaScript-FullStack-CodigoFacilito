63390190808a2da56ad6a38b918edfc34ec53f9c
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-21 03:21:52 -0300

    Se agrega ejercicio booleanos

---
+++ Booleanos.js
@@ -0,0 +1,7 @@
+let notificacionesActivas = true;
+let recibirCorreos = false;
+let activoEnChat = true;
+
+let booleano = new Boolean("false");
+console.log(booleano.toString());
+undefined, NaN, 0, .0, "", false;
