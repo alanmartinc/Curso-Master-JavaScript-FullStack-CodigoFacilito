015934656d98cbb399f257632c1a8656e62a388e
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-21 03:29:11 -0300

    Se agrega ejercicio condicionales

---
+++ Condicionales.js
@@ -0,0 +1,19 @@
+let edad = 10;
+
+if(edad >= 18) {
+  console.log("Eres mayor de edad");
+} else {
+  console.log("Eres menor de edad");
+}
+
+let calificacion = 9;
+
+if (calificacion == 10) {
+  console.log("Excelente");
+} else if (calificacion > 7) {
+  console.log("Muy bien");
+} else if (calificacion > 5) {
+  console.log("Puedes mejorar");
+} else {
+  console.log("Reprobado");
+}
