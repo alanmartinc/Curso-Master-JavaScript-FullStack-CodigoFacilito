adc80d5048267b89e6e6cd4e8df17c83888f0a97
zaculizate1996 <alancabot.ac@gmail.com>
2020-03-21 04:01:13 -0300

    Se agrega ejercicio arrow functions

---
+++ ArrowFunctions.js
@@ -0,0 +1,10 @@
+let demo = ()=> {
+  console.log("Hola mundo");
+}
+
+demo();
+
+let saludar = ()=> console.log("Hola Mundo"); // Se puede omitir las llaves
+
+suma = (a,b)=> a + b
+console.log(suma(2,3));
